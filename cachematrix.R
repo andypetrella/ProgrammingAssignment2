fbba65fc4d6d0c089a251348e4c968e7b31ef95c
noootsab <andy.petrella@gmail.com>
2014-04-13 22:31:59 +0200

    first try for the first assigneent

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This script provides some utils to memoize the inversion of a matrix
 
-## Write a short comment describing this function
+## This function is responsible to hold the reference to the current
+## matrix and to the potentially (hence lazily memoized) computed inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  .inv <- NULL
+  set <- function(y) {
+    x <<- y
+    .inv <<- NULL
+  }
+  get <- function() x
+  set.inv <- function(inv) .inv <<- inv
+  get.inv <- function() .inv
+  list(set = set, get = get,
+       set.inv = set.inv,
+       get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
-
+## This function is actually performing the lazy memoization by checking
+## if there is a memoized value in x (begin a list returned by makeCacheMatrix)
+## If none (NULL), the inverse of x is computed and memoized in x.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$get.inv()
+  if(is.null(inv)) {
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$set.inv(inv)
+  } else {
+    message("getting cached inverse matrix")
+  }
+  inv
 }
